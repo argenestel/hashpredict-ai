6aacda15c1f235d5706471b6e043af139cad0063
argenestel <arpitpali68@gmail.com>
2024-10-10 20:26:41 +0530

    Move redeployed

--- frontend/src/components/navbar/index.tsx
+++ frontend/src/components/navbar/index.tsx
@@ -215,7 +215,7 @@ const NavBar = ({ isMobile }) => {
             initial={{ opacity: 0 }}
             animate={{ opacity: 1 }}
             exit={{ opacity: 0 }}
-            className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center p-4 z-70"
+            className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center p-4 z-50"
           >
             <motion.div 
               initial={{ scale: 0.9, opacity: 0 }}
